4fc46a967771e072bc56cec57300ed40c7ef03cd
Ladislus <walcak.ladislas@gmail.com>
2022-12-01 20:46:31 +0100

    [ADD] Added part 1

---
+++ main.py
@@ -0,0 +1,32 @@
+from sys import argv, exit
+
+def help():
+    print(f'python3 {argv[0]} input_file_path')
+    exit(1)
+
+def read_file(filepath: str) -> list[int]:
+
+    elves: list[int] = []
+    calories: int = 0
+
+    with open(filepath) as file:
+        for line in file.readlines():
+            if line == '\n':
+                if calories != 0:
+                    elves.append(calories)
+                    calories = 0
+            else:
+                calories += int(line)
+
+    return elves
+
+
+if __name__ == '__main__':
+
+    if len(argv) != 2:
+        help()
+
+    elves: list[int] = read_file(argv[1])
+    print(max(elves))
+
+
