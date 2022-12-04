c795fdb7ae834851b68571b93505c6eb203d5e24
Ladislus <walcak.ladislas@gmail.com>
2022-12-04 19:27:38 +0100

    [ADD] Added Day 4 - Part 2

--- 4/src/main.rs
+++ 4/src/main.rs
@@ -2,8 +2,9 @@ use std::env;
 use std::process::exit;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use std::collections::HashSet;
 
-fn open_input() {
+fn input_v1() {
     let filepath: String = env::args().nth(1).unwrap();
     if let Ok(file) = File::open(&filepath) {
 
@@ -11,9 +12,8 @@ fn open_input() {
 
         let mut score: u32 = 0;
 
-        for (index, line) in buf_reader.lines().enumerate() {
+        for line in buf_reader.lines(){
             if let Ok(line) = line {
-                println!("Line {}: \"{}\"", index, line);
                 let splitted: Vec<&str> = line.split(",").collect();
 
                 if splitted.len() != 2 {
@@ -33,12 +33,10 @@ fn open_input() {
                     exit(1);
                 }
 
-                let left_bot = left_split[0].parse::<u32>().unwrap();
-                let left_top = left_split[1].parse::<u32>().unwrap();
-                let right_bot = right_split[0].parse::<u32>().unwrap();
-                let right_top = right_split[1].parse::<u32>().unwrap();
-
-                println!("{}-{},{}-{}", left_bot, left_top, right_bot, right_top);
+                let left_bot: u32 = left_split[0].parse::<u32>().unwrap();
+                let left_top: u32 = left_split[1].parse::<u32>().unwrap();
+                let right_bot: u32 = right_split[0].parse::<u32>().unwrap();
+                let right_top: u32 = right_split[1].parse::<u32>().unwrap();
 
                 if ((left_bot <= right_bot) && (left_top >= right_top)) || ((right_bot <= left_bot) && (right_top >= left_top)) { score += 1; }
 
@@ -56,6 +54,57 @@ fn open_input() {
     }
 }
 
+fn input_v2() {
+    let filepath: String = env::args().nth(1).unwrap();
+    if let Ok(file) = File::open(&filepath) {
+
+        let buf_reader: BufReader<File> = BufReader::new(file);
+
+        let mut score: u32 = 0;
+
+        for line in buf_reader.lines() {
+            if let Ok(line) = line {
+                let splitted: Vec<&str> = line.split(",").collect();
+
+                if splitted.len() != 2 {
+                    eprintln!("Vec didn't split into two parts {:?}", splitted);
+                    exit(1);
+                }
+
+                let left_split: Vec<&str> = splitted[0].split("-").collect();
+                if left_split.len() != 2 {
+                    eprintln!("Left split didn't split into two parts {:?}", splitted);
+                    exit(1);
+                }
+
+                let right_split: Vec<&str> = splitted[1].split("-").collect();
+                if right_split.len() != 2 {
+                    eprintln!("Right split didn't split into two parts {:?}", splitted);
+                    exit(1);
+                }
+
+                let left_bot: u32 = left_split[0].parse::<u32>().unwrap();
+                let left_top: u32 = left_split[1].parse::<u32>().unwrap();
+                let right_bot: u32 = right_split[0].parse::<u32>().unwrap();
+                let right_top: u32 = right_split[1].parse::<u32>().unwrap();
+
+                let left_set: HashSet<u32> = (left_bot..left_top + 1).collect();
+                let right_set: HashSet<u32> = (right_bot..right_top + 1).collect();
+
+                if !left_set.is_disjoint(&right_set) { score += 1; }
+            } else {
+                eprintln!("Error: \"Error while reading lines\"");
+                exit(1);
+            }
+        }
+
+        println!("Score: {}", score);
+    } else {
+        eprintln!("Couldn't open filepath {}", filepath);
+        exit(1);
+    }
+}
+
 fn help() {
     eprintln!("Usage: {} input_file_path", env::args().nth(0).unwrap());
     exit(1);
@@ -63,5 +112,6 @@ fn help() {
 
 fn main() {
     if env::args().len() != 2 { help(); }
-    open_input();
+    input_v1();
+    input_v2();
 }
