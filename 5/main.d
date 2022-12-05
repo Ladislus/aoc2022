1f91edb414051cf62063b08dd78fc865f158a7d9
Ladislus <walcak.ladislas@gmail.com>
2022-12-05 19:38:04 +0100

    [ADD] Added Day 5 - Part 1

--- 5/main.d
+++ 5/main.d
@@ -1,11 +1,15 @@
-import std.stdio: writeln, File, stderr;
+import std.stdio: writeln, write, File, stderr;
 import core.stdc.stdlib: exit;
 import std.exception: ErrnoException;
 import std.container: DList, Array;
-import std.ascii: newline;
-import std.string: strip;
-import std.array: split, array, empty, minimallyInitializedArray;
-import std.range: stride;
+import std.ascii: newline, isWhite;
+import std.string: strip, isNumeric;
+import std.algorithm.searching: endsWith;
+import std.array: split, array;
+import std.range: stride, walkLength;
+import std.range.primitives: popFront;
+import std.regex: regex, matchFirst;
+import std.conv: to;
 
 void help(string program_name) {
 	stderr.writeln("Usage: ", program_name, " input_file_path");
@@ -14,44 +18,129 @@ void help(string program_name) {
 
 void input_v1(string filepath) {
 
-	Array!(DList!char) stacks = Array!(DList!char)();
-	stacks.insertAfter(DList!char());
-	stderr.writefln("Stack size: ", stacks.length);
-	exit(1);
+	auto stacks = Array!(DList!dchar)();
 
 	try {
 		File file = File(filepath, "r");
 
 		string current_line;
-		while ((current_line = strip(file.readln())) !is null) {
-			writeln("\"", current_line, "\"");
+		while ((current_line = file.readln()) !is null) {
 
-			if (current_line.length == 0) break;
+			if (strip(current_line).length == 0) break;
 
 			dchar[] splitted = stride(current_line[1 .. $ - 1].dup, 4).array();
-			
+
 			if (splitted.length == 0) {
 				stderr.writeln("Empty stride");
 				exit(1);
 			}
 
-			if (splitted.length > stacks.length) {
-				auto diff = splitted.length - stacks.length;
-				// foreach (i; 0..diff) stacks.insertAfter(DList!char());
-			}
+			// Skip line with numbers
+			if (isNumeric(splitted)) continue;
 
-			writeln("\"", splitted, "\"");
+			if (splitted.length > stacks.length)
+				foreach (i; stacks.length .. splitted.length)
+					stacks.insert(DList!dchar());
+
+			foreach (i; 0..splitted.length)
+				if (!isWhite(splitted[i]))
+					stacks[i].insertFront(splitted[i]);
 		}
 
-		// while ((current_line = strip(file.readln())) !is null) {
-		// 	writeln("\"", current_line, "\"");
-		// }
+		auto reg = regex(r"^move (?P<count>\d+) from (?P<from>\d+) to (?P<to>\d+)$");
+		while ((current_line = strip(file.readln())) !is null) {
+
+			auto match = matchFirst(current_line, reg);
+			assert(match.length == 4);
+
+			const auto count = (match["count"].to!int());
+			const auto from = (match["from"].to!int()) - 1;
+			const auto to = (match["to"].to!int()) - 1;
+
+			foreach (i; 0 .. count) {
+				assert(from < stacks.length && to < stacks.length && from != to);
+				if (walkLength(stacks[from][]) == 0) { writeln("Empty stack, breaking"); break; }
+
+				auto popped = stacks[from].back;
+				stacks[from].removeBack();
+				stacks[to].insertBack(popped);
+			}
+		}
 
 		file.close();
 
 		string result = "";
-		foreach (DList!char key; stacks) {
-			result ~= key.back();
+		foreach (DList!dchar key; stacks) {
+			if (walkLength(key[]) == 0) result ~= " ";
+			else result ~= key.back();
+		}
+
+		writeln("result v1: \"", result, "\"");
+
+	} catch	(ErrnoException ex) {
+		stderr.writeln("Error while openning file ", filepath, " with error:");
+		stderr.writeln(ex.msg);
+		exit(1);
+	}
+}
+
+void input_v2(string filepath) {
+
+	auto stacks = Array!(DList!dchar)();
+
+	try {
+		File file = File(filepath, "r");
+
+		string current_line;
+		while ((current_line = file.readln()) !is null) {
+
+			if (strip(current_line).length == 0) break;
+
+			dchar[] splitted = stride(current_line[1 .. $ - 1].dup, 4).array();
+
+			if (splitted.length == 0) {
+				stderr.writeln("Empty stride");
+				exit(1);
+			}
+
+			// Skip line with numbers
+			if (isNumeric(splitted)) continue;
+
+			if (splitted.length > stacks.length)
+				foreach (i; stacks.length .. splitted.length)
+					stacks.insert(DList!dchar());
+
+			foreach (i; 0..splitted.length)
+				if (!isWhite(splitted[i]))
+					stacks[i].insertFront(splitted[i]);
+		}
+
+		auto reg = regex(r"^move (?P<count>\d+) from (?P<from>\d+) to (?P<to>\d+)$");
+		while ((current_line = strip(file.readln())) !is null) {
+
+			auto match = matchFirst(current_line, reg);
+			assert(match.length == 4);
+
+			const auto count = (match["count"].to!int());
+			const auto from = (match["from"].to!int()) - 1;
+			const auto to = (match["to"].to!int()) - 1;
+
+			foreach (i; 0 .. count) {
+				assert(from < stacks.length && to < stacks.length && from != to);
+				if (walkLength(stacks[from][]) == 0) { writeln("Empty stack, breaking"); break; }
+
+				auto popped = stacks[from].back;
+				stacks[from].removeBack();
+				stacks[to].insertBack(popped);
+			}
+		}
+
+		file.close();
+
+		string result = "";
+		foreach (DList!dchar key; stacks) {
+			if (walkLength(key[]) == 0) result ~= " ";
+			else result ~= key.back();
 		}
 
 		writeln("result v1: \"", result, "\"");
@@ -68,4 +157,5 @@ void main(string[] args) {
 	if (args.length != 2) help(args[0]);
 
 	input_v1(args[1]);
+	input_v2(args[1]);
 }
