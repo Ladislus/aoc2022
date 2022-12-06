fe78ab14d92dccbb7c197aa43e598d7efcefafa4
Ladislus <walcak.ladislas@gmail.com>
2022-12-06 15:14:09 +0100

    [MOD] Small refactor of Day 6 - Part 1

--- 6/main.lua
+++ 6/main.lua
@@ -1,35 +1,39 @@
--- #arg dosen't return the size of the array, but the maximum index, so size - 1
-if #arg ~= 1 then io.stderr:write(string.format("Usage: %s input_file_path\n", arg[0])) end
+function input_v1(filepath)
 
-file = io.open(arg[1], 'r')
-lines = file:lines()
+    file = io.open(filepath, 'r')
+    lines = file:lines()
 
-for line in lines do
+    for line in lines do
 
-    from = 1
-    to = 4
+        from = 1
+        to = 4
 
-    repeat
+        repeat
 
-        cur = string.sub(line, from, to)
+            cur = string.sub(line, from, to)
 
-        for char in cur:gmatch"." do
-            _, count = string.gsub(cur, char, "")
-            if count > 1 then goto next_sbustring end
-        end
+            for char in cur:gmatch"." do
+                _, count = string.gsub(cur, char, "")
+                if count > 1 then goto next_sbustring end
+            end
 
-        print(string.format("Found substring \"%s\" without duplicate at [%d, %d]", cur, from, to))
-        goto next_input
+            print(string.format("Found substring \"%s\" without duplicate at [%d, %d]", cur, from, to))
+            goto next_input
 
-        ::next_sbustring::
+            ::next_sbustring::
 
-        from = from + 1
-        to = to + 1
+            from = from + 1
+            to = to + 1
 
-    until to == (#line + 1)
+        until to == (#line + 1)
 
-    ::next_input::
+        ::next_input::
+
+    end
+
+    io.close(file)
 
 end
 
-io.close(file)
+if #arg ~= 1 then io.stderr:write(string.format("Usage: %s input_file_path\n", arg[0])) end
+input_v1(arg[1])
